6bac19a0be52c1a1c71ffd0588a44c2d22c1dc0c
Pratyush Narain <32239181+Pratyush1197@users.noreply.github.com>
2019-08-05 23:05:08 +0530

    Add files via upload

---
+++ SRIP/library/trans.js
@@ -0,0 +1,12 @@
+require(requirejs);
+const translate = require('translate-api');
+ 
+let transUrl = 'https://nodejs.org/en/';
+translate.getPage(transUrl).then(function(htmlStr){
+  console.log(htmlStr.length)
+});
+
+let transText = 'hello world!';
+translate.getText(transText,{to: 'zh-CN'}).then(function(text){
+  console.log(text)
+});
