b1a58db47ea7b682c986b69f53ef4fc87e805012
Pratyush Narain <32239181+Pratyush1197@users.noreply.github.com>
2019-08-28 21:27:54 +0530

    Update hello.py

--- SRIP_1/Codes/hello.py
+++ SRIP_1/Codes/hello.py
@@ -3,3 +3,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import wave
 import sys
+
+CHANNELS = 2
+swidth = 4
+Change_RATE = 2
+T = 10
+bits = int(sys.argv[3])
+freq = int(sys.argv[2])
+T = int(sys.argv[4])
+audio = sys.argv[1]
+#print(source)
+q = 0.5
+Q = 1/10
+A = 1 # amplitude of signal # quatization stepsize
+N = 2000
+spf = wave.open(audio, 'rb')
+CHANNELS=spf.getnchannels()
+swidth = spf.getsampwidth()
+RATE=spf.getframerate()
+print(RATE)
+Byte = spf.getsampwidth()
+signal = spf.readframes(-1)
+signal = np.fromstring(signal, 'Int16')
+spf.close()
