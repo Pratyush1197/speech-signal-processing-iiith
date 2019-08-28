5201c8f54f627237f11caaf58f5fc5d0b5a0dc62
Pratyush Narain <32239181+Pratyush1197@users.noreply.github.com>
2019-08-28 21:33:00 +0530

    Update hel.py

--- SRIP_1/Codes/hel.py
+++ SRIP_1/Codes/hel.py
@@ -7,3 +7,18 @@ import contextlib
 
 fname = sys.argv[1]
 outname = sys.argv[1]
+
+cutOffFrequency = 400.0
+  cumsum = np.cumsum(np.insert(x, 0, 0)) 
+  return (cumsum[windowSize:] - cumsum[:-windowSize]) / windowSize
+
+def interpret_wav(raw_bytes, n_frames, n_channels, sample_width, interleaved = True):
+
+    if sample_width == 1:
+        dtype = np.uint8 # unsigned char
+    elif sample_width == 2:
+        dtype = np.int16 # signed 2-byte short
+    else:
+        raise ValueError("Only supports 8 and 16 bit audio formats.")
+
+    channels = np.fromstring(raw_bytes, dtype=dtype)
