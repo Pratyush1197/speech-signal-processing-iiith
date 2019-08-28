fc9134bf96519bebb0e0c97ef9025fea413e326d
Pratyush Narain <32239181+Pratyush1197@users.noreply.github.com>
2019-08-28 21:29:59 +0530

    Update hello.py

--- SRIP_1/Codes/hello.py
+++ SRIP_1/Codes/hello.py
@@ -33,3 +33,44 @@ wf.setsampwidth(swidth)
 wf.setframerate(freq)
 wf.writeframes(signal)
 wf.close()
+
+def uniform_midtread_quantizer(x, Q):
+    # limiter
+    x = np.copy(x)
+
+    idx = np.where(np.abs(x) >= 1)
+    x[idx] = np.sign(x[idx])
+    # linear uniform quantization
+    xQ = Q * np.floor(x/Q + 1/2)
+
+    return xQ
+
+def plot_signals(x, xQ, T):
+    e = xQ - x
+    plt.figure(figsize=(10,6))
+    plt.plot(xQ, label=r'quantized signal $x_Q[k]$')
+    plt.xlabel(r'$k$')
+    plt.axis([0, N, -T, T])
+    plt.grid()
+    plt.savefig('my_plot.png')
+
+def plot_signalss(x, xQ, T):
+    e = xQ - x
+    plt.figure(figsize=(10,6))
+    plt.plot(signal, label=r'quantized signal $x_Q[k]$')
+    plt.xlabel(r'$k$')
+    plt.axis([0, N, -T, T])
+    plt.grid()
+    plt.savefig('my_plot.png')
+
+# generate signal
+x = signal
+# quantize signal
+xQ = uniform_midtread_quantizer(x, Q)
+# plot signals
+if (bits < 3):
+    plot_signals(x, xQ, T)
+else:
+    plot_signalss(x, xQ, T)    
+
+
